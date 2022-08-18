c9538d04061753eb9efab552b935093e0c37e7ae
Nate Catelli <ncatelli@packetfire.org>
2022-08-18 03:27:48 +0000

    add additional serialization test

--- tests/src/serialization_deserialization.rs
+++ tests/src/serialization_deserialization.rs
@@ -3,6 +3,59 @@ use regex_runtime::bytecode::FromBytecode;
 use regex_runtime::*;
 
 #[test]
+fn should_preserve_expected_semantics_from_parse_through_serialization() {
+    use regex_runtime::{CharacterAlphabet, CharacterSet, FastForward, SetMembership};
+    let expr = "\"([a-z]+)\"";
+
+    let insts = regex_compiler::parse(expr)
+        .map_err(|e| format!("{:?}", e))
+        .and_then(regex_compiler::compile)
+        .unwrap();
+
+    let expected_program = Instructions::new(
+        vec![CharacterSet::new(
+            SetMembership::Inclusive,
+            CharacterAlphabet::Range('a'..='z'),
+        )],
+        vec![
+            Opcode::Split(InstSplit::new(InstIndex::from(3), InstIndex::from(1))),
+            Opcode::Any,
+            Opcode::Jmp(InstJmp::new(InstIndex::from(0))),
+            Opcode::Consume(InstConsume::new('"')),
+            Opcode::StartSave(InstStartSave::new(0)),
+            Opcode::ConsumeSet(InstConsumeSet::new(0)),
+            Opcode::Split(InstSplit::new(InstIndex::from(7), InstIndex::from(9))),
+            Opcode::ConsumeSet(InstConsumeSet::new(0)),
+            Opcode::Jmp(InstJmp::new(InstIndex::from(6))),
+            Opcode::EndSave(InstEndSave::new(0)),
+            Opcode::Consume(InstConsume::new('"')),
+            Opcode::Match,
+        ],
+    )
+    .with_fast_forward(FastForward::Char('"'));
+
+    assert_eq!(&insts, &expected_program);
+
+    let generated_bytecode = insts.to_bytecode();
+
+    let deserialized_insts =
+        regex_runtime::Instructions::from_bytecode(generated_bytecode).unwrap();
+
+    assert_eq!(&insts, &deserialized_insts);
+
+    let input = "\"hello\"";
+    assert_eq!(
+        Some([SaveGroupSlot::complete(1, 6)]),
+        regex_runtime::run::<1>(&insts, input)
+    );
+
+    assert_eq!(
+        Some([SaveGroupSlot::complete(1, 6)]),
+        regex_runtime::run::<1>(&deserialized_insts, input)
+    );
+}
+
+#[test]
 fn should_preserve_equivalent_representation_in_bytecode_encoding() {
     let input_output = [
         Instructions::new(vec![], vec![Opcode::Any, Opcode::Match]),

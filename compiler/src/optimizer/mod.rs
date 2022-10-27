4a683de515070e8257b631f1bdfaeff95ddcce6d
Nate Catelli <ncatelli@packetfire.org>
2022-10-27 19:37:34 +0000

    prune down states

--- compiler/src/optimizer/mod.rs
+++ compiler/src/optimizer/mod.rs
@@ -1,6 +1,6 @@
 use std::collections::{HashMap, HashSet};
 
-use regex_runtime::{Instruction, Instructions, Opcode};
+use regex_runtime::{InstJmp, Instruction, Instructions, Opcode};
 
 mod directed_graph;
 use directed_graph::{DirectedGraph, Graph};
@@ -95,99 +95,67 @@ impl<'a> Thread<'a> {
     }
 }
 
-fn graph_from_bytecode_program(program: &Instructions) -> Result<AttributeGraph, String> {
-    let entry_state_id = usize::MAX;
-    let initial_thread = Thread::new(vec![entry_state_id], &program.program[0..]);
-
-    let mut current_thread_list = vec![initial_thread];
-    let mut next_thread_list = vec![];
+fn graph_from_bytecode(program: &Instructions) -> Result<AttributeGraph, String> {
     let mut graph = {
         let mut graph = AttributeGraph::new();
-        let entry_state = State::new(entry_state_id, AcceptState::NonAcceptor, 0);
 
-        graph.add_node(entry_state);
+        for inst in &program.program {
+            let state_id = inst.offset;
+            let next_state = State::new(state_id, AcceptState::NonAcceptor, 0);
+
+            graph.add_node(next_state);
+        }
+
         graph
     };
 
-    while !current_thread_list.is_empty() {
-        'thread: for thread in current_thread_list.iter_mut() {
-            for inst in thread.instructions {
-                let stack = &mut thread.stack;
-                // instruction offset, used as a state id.
-                let state_id = inst.offset;
-                let parent_state_id = stack
-                    .last()
-                    .copied()
-                    // This should never possibly occur.
-                    .ok_or_else(|| "state stack is empty".to_string())?;
-                let (parent_expr_id, _parent_save_group_id) = graph
+    for inst in &program.program {
+        let src_state_id = inst.offset;
+        match inst.opcode {
+            Opcode::Any => {
+                let dest_state_id = src_state_id + 1;
+
+                graph
+                    .graph
+                    .add_edge(src_state_id, dest_state_id, Edge::Any)
+                    .map_err(|e| e.to_string())?;
+            }
+            Opcode::Consume(regex_runtime::InstConsume { value }) => {
+                let dest_state_id = src_state_id + 1;
+                let set = [value].into_iter().collect();
+
+                graph
+                    .graph
+                    .add_edge(src_state_id, dest_state_id, Edge::MustMatchOneOf(set))
+                    .map_err(|e| e.to_string())?;
+            }
+            Opcode::ConsumeSet(_) => todo!(),
+            Opcode::Epsilon(_) => todo!(),
+            Opcode::Split(regex_runtime::InstSplit { x_branch, y_branch }) => {
+                let x_branch_state_id = x_branch.as_usize();
+                let y_branch_state_id = y_branch.as_usize();
+
+                graph
+                    .graph
+                    .add_edge(src_state_id, x_branch_state_id, Edge::Epsilon)
+                    .map_err(|e| e.to_string())?;
+                graph
+                    .graph
+                    .add_edge(src_state_id, y_branch_state_id, Edge::Epsilon)
+                    .map_err(|e| e.to_string())?;
+            }
+            Opcode::Jmp(_) => todo!(),
+            Opcode::StartSave(_) => todo!(),
+            Opcode::EndSave(_) => todo!(),
+            Opcode::Meta(_) => todo!(),
+            Opcode::Match => {
+                let terminal_state_id = src_state_id;
+                graph
                     .states_attrs
-                    .get(&parent_state_id)
-                    .map(|node| (node.expr_id, node.save_group_id))
-                    .ok_or_else(|| "unknown state".to_string())?;
-
-                match inst.opcode {
-                    Opcode::Any => {
-                        let new_state =
-                            State::new(state_id, AcceptState::NonAcceptor, parent_expr_id);
-                        graph.add_node(new_state);
-                        stack.push(state_id);
-
-                        let new_edge = Edge::Any;
-                        graph
-                            .graph
-                            .add_edge(parent_state_id, state_id, new_edge)
-                            .map_err(|e| e.to_string())?;
-                    }
-                    Opcode::Consume(regex_runtime::InstConsume { value }) => {
-                        let new_state =
-                            State::new(state_id, AcceptState::NonAcceptor, parent_expr_id);
-
-                        graph.add_node(new_state);
-                        stack.push(state_id);
-
-                        let set = [value].into_iter().collect();
-                        let new_edge = Edge::MustMatchOneOf(set);
-                        graph
-                            .graph
-                            .add_edge(parent_state_id, state_id, new_edge)
-                            .map_err(|e| e.to_string())?;
-                    }
-                    Opcode::ConsumeSet(_) => {
-                        todo!()
-                    }
-                    Opcode::Epsilon(_) => todo!(),
-                    Opcode::Split(regex_runtime::InstSplit { x_branch, y_branch }) => {
-                        let x = x_branch.as_usize();
-                        let y = y_branch.as_usize();
-                        let x_opcode_start = &program.program[x..];
-                        let y_opcode_start = &program.program[y..];
-
-                        next_thread_list.push(Thread::new(stack.clone(), x_opcode_start));
-                        next_thread_list.push(Thread::new(stack.clone(), y_opcode_start));
-                        break 'thread;
-                    }
-                    Opcode::Jmp(_) => {}
-                    Opcode::StartSave(_) => todo!(),
-                    Opcode::EndSave(_) => todo!(),
-                    Opcode::Meta(regex_runtime::InstMeta(
-                        regex_runtime::MetaKind::SetExpressionId(_),
-                    )) => {
-                        todo!()
-                    }
-                    Opcode::Match => {
-                        graph
-                            .states_attrs
-                            .entry(parent_state_id)
-                            .and_modify(|node| node.kind = AcceptState::Acceptor);
-                        break;
-                    }
-                }
+                    .entry(terminal_state_id)
+                    .and_modify(|state| state.kind = AcceptState::Acceptor);
             }
         }
-
-        std::mem::swap(&mut current_thread_list, &mut next_thread_list);
-        next_thread_list.clear();
     }
 
     Ok(graph)
@@ -199,22 +167,10 @@ mod tests {
     use regex_runtime::*;
 
     #[test]
-    fn empty_program_should_generate_initial_state() {
-        let program = Instructions::new(vec![], vec![]);
-        let res = graph_from_bytecode_program(&program);
-
-        assert!(res.is_ok());
-
-        // safe to unwrap with above assertion.
-        let graph = res.unwrap();
-        assert_eq!(1, graph.graph.nodes().len());
-    }
-
-    #[test]
     fn should_generate_terminal_state_for_consuming_instructions() {
         let opcodes = vec![Opcode::Any, Opcode::Any, Opcode::Match];
         let program = Instructions::new(vec![], opcodes);
-        let res = graph_from_bytecode_program(&program);
+        let res = graph_from_bytecode(&program);
 
         assert!(res.is_ok());
 
@@ -226,8 +182,8 @@ mod tests {
         assert_eq!(
             vec![
                 &State::new(0, AcceptState::NonAcceptor, 0),
-                &State::new(1, AcceptState::Acceptor, 0),
-                &State::new(usize::MAX, AcceptState::NonAcceptor, 0),
+                &State::new(1, AcceptState::NonAcceptor, 0),
+                &State::new(2, AcceptState::Acceptor, 0),
             ],
             states
         )
@@ -243,7 +199,7 @@ mod tests {
             Opcode::Match,
         ];
         let program = Instructions::new(vec![], opcodes);
-        let res = graph_from_bytecode_program(&program);
+        let res = graph_from_bytecode(&program);
 
         // safe to unwrap with above assertion.
         let graph = res.unwrap();
@@ -252,8 +208,8 @@ mod tests {
 
         assert_eq!(
             vec![
-                DirectedEdge::new(&0, &1, &Edge::MustMatchOneOf(['a'].into_iter().collect())),
-                DirectedEdge::new(&usize::MAX, &0, &Edge::Any),
+                DirectedEdge::new(&0, &1, &Edge::Any),
+                DirectedEdge::new(&1, &2, &Edge::MustMatchOneOf(['a'].into_iter().collect())),
             ],
             edges
         )
@@ -273,7 +229,7 @@ mod tests {
             Opcode::Match,
         ];
         let program = Instructions::new(vec![], opcodes);
-        let res = graph_from_bytecode_program(&program);
+        let res = graph_from_bytecode(&program);
 
         // safe to unwrap with above assertion.
         let graph = res.unwrap();
@@ -282,15 +238,17 @@ mod tests {
 
         assert_eq!(
             vec![
+                DirectedEdge::new(&0, &1, &Edge::Epsilon),
+                DirectedEdge::new(&0, &4, &Edge::Epsilon),
                 DirectedEdge::new(&1, &2, &Edge::Any),
+                DirectedEdge::new(&2, &3, &Edge::Any),
                 DirectedEdge::new(&4, &5, &Edge::Any),
-                DirectedEdge::new(&usize::MAX, &1, &Edge::Any),
-                DirectedEdge::new(&usize::MAX, &4, &Edge::Any),
+                DirectedEdge::new(&5, &6, &Edge::Any),
             ],
             edges
         );
 
-        // entry node plus 4 consuming nodes.
-        assert_eq!(5, graph.graph.nodes().len());
+        // entry node plus one for each state.
+        assert_eq!(7, graph.graph.nodes().len());
     }
 }

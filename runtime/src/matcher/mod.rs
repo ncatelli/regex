5838b22da0e18a9ba992e47191b562ea6467b67d
Nate Catelli <ncatelli@packetfire.org>
2023-07-28 21:50:38 +0000

    feat: Implement Concatenation and Alternation

--- runtime/src/matcher/mod.rs
+++ runtime/src/matcher/mod.rs
@@ -155,5 +155,147 @@ impl<T: Eq> PatternEvaluatorMut for Literal<T> {
     }
 }
 
+/// Concatenates to matchers, matching them sequentially.
+///
+/// # Examples
+///
+/// ```
+/// use regex_runtime::matcher::*;
+///
+/// let literal_a = Literal::new('a').initial_state();
+/// let literal_b = Literal::new('b').initial_state();
+/// let mut concat = Concatenation::new(literal_a, literal_b).initial_state();
+///
+/// // Advances one character that matches the expected literal.
+/// assert!(concat.matches("ab".chars()));
+/// assert!(concat.is_in_accept_state());
+/// ```
+pub struct Concatenation<T, PE1, PE2> {
+    ty: std::marker::PhantomData<T>,
+    pe1: PE1,
+    pe2_started: bool,
+    pe2: PE2,
+}
+
+impl<T, PE1, PE2> Concatenation<T, PE1, PE2>
+where
+    PE1: PatternEvaluatorMut<Item = T>,
+    PE2: PatternEvaluatorMut<Item = T>,
+{
+    #[must_use]
+    pub fn new(pe1: PE1, pe2: PE2) -> Self {
+        Self {
+            ty: std::marker::PhantomData,
+            pe1,
+            pe2_started: false,
+            pe2,
+        }
+    }
+}
+
+impl<T, PE1, PE2> PatternEvaluatorMut for Concatenation<T, PE1, PE2>
+where
+    PE1: PatternEvaluatorMut<Item = T>,
+    PE2: PatternEvaluatorMut<Item = T>,
+{
+    type Item = T;
+
+    fn initial_state_mut(&mut self) {
+        // clear the acceptor state if set and set as in initial state.
+        self.pe1.initial_state_mut();
+
+        let pe1_in_accept_state = self.pe1.is_in_accept_state();
+        self.pe2_started = pe1_in_accept_state;
+
+        if self.pe2_started {
+            self.pe2.initial_state_mut();
+        }
+    }
+
+    fn is_in_accept_state(&self) -> bool {
+        self.pe2.is_in_accept_state()
+    }
+
+    fn advance_mut<'a>(&mut self, next: &'a Self::Item) -> Option<&'a Self::Item> {
+        if self.pe2_started {
+            self.pe2.advance_mut(next)
+        } else {
+            let pe1_advanced = self.pe1.advance_mut(next);
+            self.pe2_started = self.pe1.is_in_accept_state();
+
+            pe1_advanced
+        }
+    }
+}
+
+/// Matches either of two sub-matchers.
+///
+/// # Examples
+///
+/// ```
+/// use regex_runtime::matcher::*;
+///
+/// let literal_a = Literal::new('a').initial_state();
+/// let literal_b = Literal::new('b').initial_state();
+/// let mut alt = Alternation::new(literal_a, literal_b).initial_state();
+///
+/// // matches either a | b.
+/// assert!(alt.matches("a".chars()));
+/// assert!(alt.is_in_accept_state());
+///
+/// alt.initial_state_mut();
+/// assert!(alt.matches("b".chars()));
+/// assert!(alt.is_in_accept_state());
+///
+/// alt.initial_state_mut();
+/// assert!(!alt.matches("c".chars()));
+/// assert!(!alt.is_in_accept_state());
+///
+/// ```
+pub struct Alternation<T, PE1, PE2> {
+    ty: std::marker::PhantomData<T>,
+    pe1: PE1,
+    pe2: PE2,
+}
+
+impl<T, PE1, PE2> Alternation<T, PE1, PE2>
+where
+    PE1: PatternEvaluatorMut<Item = T>,
+    PE2: PatternEvaluatorMut<Item = T>,
+{
+    pub fn new(pe1: PE1, pe2: PE2) -> Self {
+        Self {
+            ty: std::marker::PhantomData,
+            pe1,
+            pe2,
+        }
+    }
+}
+impl<T, PE1, PE2> PatternEvaluatorMut for Alternation<T, PE1, PE2>
+where
+    PE1: PatternEvaluatorMut<Item = T>,
+    PE2: PatternEvaluatorMut<Item = T>,
+{
+    type Item = T;
+
+    fn initial_state_mut(&mut self) {
+        self.pe1.initial_state_mut();
+        self.pe2.initial_state_mut();
+    }
+
+    fn is_in_accept_state(&self) -> bool {
+        self.pe1.is_in_accept_state() || self.pe2.is_in_accept_state()
+    }
+
+    fn advance_mut<'a>(&mut self, next: &'a Self::Item) -> Option<&'a Self::Item> {
+        let pe1_advanced = self.pe1.advance_mut(next);
+        if pe1_advanced.is_some() {
+            pe1_advanced
+        } else {
+            self.pe2.advance_mut(next)
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {}
